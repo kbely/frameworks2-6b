44b0c0e945ffe9f5838ca31bbef7e4776b6f65c2
kbely <119938907+kbely@users.noreply.github.com>
2024-09-27 14:21:55 +0000

    aula27092

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
+import Footer from "./component/footer";
+import Navbar from "./component/navbar";
 
 function Index(){
-  return <h1>ola mundo</h1>
+  return 
+  <div> 
+   <Navbar/>
+   <Footer/> 
+      <h1>ola mundo</h1>
+
+  </div>
+  
 }
 export default Index;
